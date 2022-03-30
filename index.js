53e867866ede07801ef132d55a894519f525067d
otienobrax <otienobraxtonn@gmail.com>
2022-03-30 23:13:46 +0300

    worked on index.js to render static files

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 //import express from 'express';//es6
 //import path from 'path';
-const express = require('express')
-const path = require('path')
-var dirname = path.resolve("./");
-const staticDir = path.join(dirname, `src`);
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const port = process.env.PORT || 3001;
 const app = express();
-app.use(express.static(staticDir));//set static directory
+app.use(express.static(path.join(__dirname, '.')));//set static directory
 
 app.get("/",(req,res)=>{
     //handle home route 
-    console.log(req.url);//debug log
-    res
-        .status(202)
-        .sendFile(path.join(`${staticDir}/html/CYBARX- Your security patner.html`))
-})
+    res.sendFile('index.html',{root:'.'});
+});
 app.listen(port,()=>{
     console.log(`listening on port ${port}`)//debug log 
-})
+});
